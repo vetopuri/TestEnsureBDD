81f7898b38eaeb007d1442f94f787f68bb784cb7
venugopal.topuri <venugopal.topuri@sailotech.com>
2024-02-21 14:49:53 +0530

    WebDriverManager&POM

--- src/test/java/stepDefinition/LoginStepDef.java
+++ src/test/java/stepDefinition/LoginStepDef.java
@@ -28,6 +28,7 @@ import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import pageObject.LoginPage;
 import pageObject.PhysicalProgressPage;
 
